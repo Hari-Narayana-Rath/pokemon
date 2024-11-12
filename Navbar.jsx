88afc21b0229178c9499857c8aa3f70c61c2cc17
Hari Narayana Rath <119601501+Hari-Narayana-Rath@users.noreply.github.com>
2024-11-12 12:25:23 +0530

    Update Navbar.jsx

--- Navbar.jsx
+++ Navbar.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Navbar.css';
+import pokemonLogo from './pokemon-logo.png'; // Import the local image file
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <img
-        src="https://i.pinimg.com/564x/85/9e/03/859e0396b1c628832ba6b7c47c4a95b5.jpg"
-        alt="Pokémon Logo"
+      <img 
+        src={pokemonLogo} 
+        alt="Pokémon Logo" 
+        className="logo-img" 
       />
     </nav>
   );
