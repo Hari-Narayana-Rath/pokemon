ac97d0fd07c4eeeac65f7effc2a239c1d1260359
Hari Narayana Rath <119601501+Hari-Narayana-Rath@users.noreply.github.com>
2024-11-12 12:15:10 +0530

    Update Navbar.jsx

--- Navbar.jsx
+++ Navbar.jsx
@@ -4,7 +4,7 @@ import './Navbar.css';
 function Navbar() {
   return (
     <nav className="navbar">
-      <img src="/pokemon-logo.png" alt="Pokémon Logo" className="logo-img" />
+      <img src="pokemon-logo.png" alt="Pokémon Logo" className="logo-img" />
     </nav>
   );
 }
